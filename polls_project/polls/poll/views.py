f20a61482bcfd7fc6df6512eb8a53f32bd4dc17d
Garrett <gkatashima@gmail.com>
2021-01-13 17:25:59 -0700

    added voting section

--- polls_project/polls/poll/views.py
+++ polls_project/polls/poll/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
 
 def index(request):
@@ -17,7 +19,16 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'poll/results.html', {'question': question})
 
-# def vote(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     try:
-#         selec
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'poll/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse ('poll:results', args=(question.id,)))
